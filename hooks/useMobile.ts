e93ea0af071849b5c8b9f4687a0e47748b946051
222UM222 <222UM222@users.noreply.github.com>
2025-11-19 19:53:35 -0600

    Add useIsMobile hook

---
+++ hooks/useMobile.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useIsMobile(breakpoint: number = 768) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
+
+    const listener = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
+    // Set initial state
+    setIsMobile(mediaQuery.matches);
+
+    if (typeof mediaQuery.addEventListener === "function") {
+      mediaQuery.addEventListener("change", listener);
+      return () => mediaQuery.removeEventListener("change", listener);
+    }
+
+    mediaQuery.addListener(listener);
+    return () => mediaQuery.removeListener(listener);
+  }, [breakpoint]);
+
+  return isMobile;
+}
